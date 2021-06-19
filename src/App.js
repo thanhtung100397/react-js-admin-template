889665bb3910bdbf4ce5a67ad9769d7d1b3c1fef
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-06-19 15:16:09 +0700

    Change all .css to .scss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Icons } from './assets/icons'
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="app-header">
+        <Icons.AppLogo className="app-logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="app-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
