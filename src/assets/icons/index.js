1a3fcc8db610c8a8588d94d4d112acf2f50c424a
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-06-19 11:39:47 +0700

    Create boilerplate code for app assets

---
+++ src/assets/icons/index.js
@@ -0,0 +1,10 @@
+import { ReactComponent as AppLogo } from "./ic_app_logo.svg";
+import ic_app_logo from "./ic_app_logo.svg";
+
+export const Icons = {
+  AppLogo
+}
+
+export const icons = {
+  ic_app_logo
+}
