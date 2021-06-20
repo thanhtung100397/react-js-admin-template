21d2314d5af9b5159f58cadef84c4da1066168b7
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-06-21 02:01:06 +0700

    Add components/base.js to store all base/shared metadata of app components

---
+++ src/components/base.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+export const baseProps = {
+  className: PropTypes.string,
+  style: PropTypes.object
+};
+
+export const fromBaseProps = (startedValues = {}, props) => {
+  return {
+    className: classNames(
+      {
+        [startedValues.className]: startedValues.className,
+        [props.className]: props.className
+      }
+    ),
+    style: {
+      ...startedValues.style,
+      ...props.style
+    }
+  }
+}
