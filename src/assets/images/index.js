ddf4ca4d8145d92112db301cc378cc5f5523ec47
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-06-21 01:58:25 +0700

    Add img_started_background.jpg

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,7 @@
 import img_og_image from './img_og_image.jpg'
+import img_started_background from './img_started_background.jpg'
 
 export const images = {
-  img_og_image
+  img_og_image,
+  img_started_background
 }
