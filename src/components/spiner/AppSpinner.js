1ec81e6f897320c08844ebef27c566a13a59b659
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-06-21 02:03:39 +0700

    Add 'AppSpinner' component for displaying infinity rotate animation of image

---
+++ src/components/spiner/AppSpinner.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { baseProps, fromBaseProps } from '../base';
+import PropTypes from 'prop-types';
+import check from 'check-types';
+import { LoadingOutlined } from '@ant-design/icons';
+import './AppSpinner.scss';
+
+const propTypes = {
+  ...baseProps,
+  indicator: PropTypes.node,
+  width: PropTypes.number,
+  height: PropTypes.number
+};
+
+const defaultProps = {
+  indicator: (
+    <div className="default-indicator">
+      <LoadingOutlined/>
+    </div>
+  ),
+  width: 64,
+  height: 64
+};
+
+const AppSpinner = (props) => {
+  const renderIndicator = (indicator) => {
+    if (check.string(indicator)) {
+      indicator = <img src={indicator} alt="" style={{width: props.width, height: props.height}}/>;
+    }
+    return (
+      <div className="indicator" style={{width: props.width, height: props.height}}>
+        {indicator}
+      </div>
+    )
+  }
+
+  return (
+    <div {...fromBaseProps({ className: 'app-spinner' }, props)}>
+      {renderIndicator(props.indicator)}
+    </div>
+  )
+};
+
+AppSpinner.propTypes = propTypes;
+
+AppSpinner.defaultProps = defaultProps;
+
+export default AppSpinner;
