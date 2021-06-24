4983edc795ae9263c1f8dc2a1f0370bdd8a86038
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-06-24 23:03:50 +0700

    Add 4 new checker for TypeChecker: isSet, isMap, isCollection, isUnset

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,11 +6,15 @@ export const TypeChecker = {
   isBoolean: (value) => _.isBoolean(value),
   isDate: (value) => _.is(value),
   isArray: (value) => _.isArray(value),
+  isSet: (value) => _.isSet(value),
+  isMap: (value) => _.isMap(value),
+  isCollection: (value) => TypeChecker.isArray(value) || TypeChecker.isSet(value) || TypeChecker.isMap(value),
   isObject: (value) => _.isObject(value),
   isFunction: (value) => _.isFunction(value),
   isEmpty: (value) => _.isEmpty(value), // Checks if value is an empty object, collection, map, or set.
   isError: (value) => _.isError(value),
   isElement: (value) => _.isElement(value),
+  isUnset: (value) => _.isNull(value) || _.isUndefined(value)
 }
 
 export const Generator = {
