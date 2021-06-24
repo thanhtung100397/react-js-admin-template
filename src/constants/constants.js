549060e799196c7bbdc9acd7517f0afca2e77c76
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-06-25 00:33:36 +0700

    Add Regex to constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -13,3 +13,8 @@ export const ValidateStatus = {
   ERROR: 'error',
   VALIDATING: 'validating',
 };
+
+export const Regex = {
+  EMAIL: '^[A-z0-9][A-z0-9_\\.\\+-]{0,32}@([a-z0-9_-]{1,}\\.){0,}([a-z0-9_-]{2,}\\.){0,}[a-z0-9-_]{2,}(\\.[a-z0-9_-]{2,}){1,2}$',
+  PHONE: '[0-9]{8,11}'
+}
