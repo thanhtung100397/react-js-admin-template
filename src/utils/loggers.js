3833382ead18052c0cfe8c916ef595a439087338
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-06-25 15:39:05 +0700

    Fix ConsoleLogger wrong params

--- src/utils/loggers.js
+++ src/utils/loggers.js
@@ -1,7 +1,7 @@
 
 export const ConsoleLogger = {
   enable: process.env.REACT_APP_ENABLE_CONSOLE_LOGGER || true,
-  info: (data) => ConsoleLogger.enable && console.info(data),
-  debug: (data) => ConsoleLogger.enable && console.debug(data),
-  error: (data) => ConsoleLogger.enable && console.error(data)
-}
+  info: (message, data) => ConsoleLogger.enable && console.info(message, data),
+  debug: (message, data) => ConsoleLogger.enable && console.debug(message, data),
+  error: (message, data) => ConsoleLogger.enable && console.error(message, data)
+};
