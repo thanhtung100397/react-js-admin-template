ec331a90f5e191a88fe8008d5cba9dc49637bede
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-06-30 17:55:35 +0700

    Add component AppDivider

---
+++ src/components/divider/AppDivider.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { baseProps, fromBaseProps } from '../base';
+import { Divider } from 'antd';
+import './AppDivider.scss';
+
+const propTypes = {
+  ...baseProps,
+  type: PropTypes.oneOf(['horizontal ', 'vertical']), // direction type of divider,
+  title: PropTypes.node, // divider title
+  orientation: PropTypes.oneOf(['left', 'right', 'center']), // position of title inside divider
+  dashed: PropTypes.bool, // whether line is dashed
+};
+
+const defaultProps = {
+  type: 'horizontal',
+  orientation: 'left'
+};
+
+const AppDivider = (props) => {
+  return (
+    <Divider {...fromBaseProps({className: 'app-divider'}, props)}
+             type={props.type} orientation={props.orientation}
+             dashed={props.dashed}>
+      {props.title}
+    </Divider>
+  )
+};
+
+AppDivider.propTypes = propTypes;
+
+AppDivider.defaultProps = defaultProps;
+
+export default AppDivider;
