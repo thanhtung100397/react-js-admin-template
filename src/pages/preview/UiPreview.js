fa2920ed10f5336cbf7bf54df8b9cc2cb4aa0034
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-06-30 11:51:16 +0700

    Add UI preview page to display demo  app component ui

---
+++ src/pages/preview/UiPreview.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppContainer from '../../containers/container/AppContainer';
+import './UiPreview.scss';
+
+const UiPreview = (props) => {
+  return (
+    <div className="ui-preview-page">
+      <AppContainer>
+
+      </AppContainer>
+    </div>
+  );
+};
+
+export default UiPreview;
