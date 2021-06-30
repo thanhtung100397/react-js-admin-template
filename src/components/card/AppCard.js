6fe707047e892d1e2b611f9f70d1cee7a5d88cca
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-06-30 21:20:56 +0700

    Add wFull, hFull, whFull prop to component AppCard

--- src/components/card/AppCard.js
+++ src/components/card/AppCard.js
@@ -10,6 +10,9 @@ const propTypes = {
   title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]), // title content
   hoverable: PropTypes.bool, // add lift up effect when hovering card
   noBodyPadding: PropTypes.bool, // remove padding in card body
+  hFull: PropTypes.bool,
+  wFull: PropTypes.bool,
+  whFull: PropTypes.bool
 };
 
 const defaultProps = {
@@ -17,7 +20,12 @@ const defaultProps = {
 };
 
 const AppCard = (props) => {
-  const className = classNames('app-card', { 'no-body-padding': props.noBodyPadding });
+  const className = classNames('app-card', {
+    'no-body-padding': props.noBodyPadding,
+    'w-full': props.wFull,
+    'h-full': props.hFull,
+    'wh-full': props.whFull,
+  });
   return (
     <Card {...fromBaseProps({className: className}, props)}
           title={props.title} hoverable={props.hoverable}>
