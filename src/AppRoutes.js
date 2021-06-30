8d9b9a891d7ef7ee6725a26716f457e72db9be07
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-06-30 11:51:50 +0700

    Add ui preview page to AppRoutes

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 const Login = React.lazy(() => import('./pages/login/Login'));
+const UiPreview = React.lazy(() => import('./pages/preview/UiPreview'));
 
 const AppRoutes = (props) => {
   return (
     <Switch>
+      <Route path="/preview/ui" component={UiPreview}/>
       <Route path="/login" component={Login}/>
       <Route path="/" component={Login}/>
     </Switch>
