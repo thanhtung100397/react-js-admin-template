e7c2b3b5f87fbd58a6e0935c99c722fac9d4a74d
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-07-01 23:25:30 +0700

    Refactor AppNotification

--- src/components/notification/AppNotification.js
+++ src/components/notification/AppNotification.js
@@ -1,45 +1,65 @@
 import { notification } from 'antd';
 import './AppNotification.scss';
 
-const defaultConfig = (config) => ({
+const NotificationType = {
+  INFO: 'info',
+  SUCCESS: 'success',
+  WARNING: 'warning',
+  ERROR: 'error'
+};
+
+const defaultConfig = {
   className: 'app-notification',
   duration: 2, // time in seconds before notification is closed
   placement: 'topRight', // position of notification, values 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight'
-  ...config
-});
+};
+
+const newNotification = async (type, config) => {
+  let func = notification[type];
+  if (!func) {
+    func = notification.open;
+  }
+  return new Promise((resolve, reject) => {
+    try {
+      func({
+        ...defaultConfig,
+        config,
+        onClose: () => {
+          config.onClose && config.onClose();
+          resolve();
+        }
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 const AppNotification = {
-  info: (title, message) => {
-    notification.info(defaultConfig({
-      message: title,
-      description: message
-    }));
-  },
+  info: (title, message) => newNotification(NotificationType.INFO, {
+    message: title,
+    description: message
+  }),
 
-  success: (title, message) => {
-    notification.success(defaultConfig({
-      message: title,
-      description: message
-    }));
-  },
+  success: (title, message) => newNotification(NotificationType.SUCCESS, {
+    message: title,
+    description: message
+  }),
 
-  warning: (title, message) => {
-    notification.warning(defaultConfig({
-      message: title,
-      description: message
-    }));
-  },
+  warning: (title, message) => newNotification(NotificationType.WARNING, {
+    message: title,
+    description: message
+  }),
 
-  error: (title, message) => {
-    notification.error(defaultConfig({
-      message: title,
-      description: message
-    }));
-  },
+  error: (title, message) => newNotification(NotificationType.ERROR, {
+    message: title,
+    description: message
+  }),
 
-  open: (config) => {
-    notification.open(config);
-  }
+  open: (title, message) => newNotification(undefined, {
+    message: title,
+    description: message
+  }),
 };
 
 export default AppNotification;
