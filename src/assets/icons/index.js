ba83e9f338df6a3a78d67860713a88945d0b3af9
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-07-03 12:33:36 +0700

    Add KeyOutlined to asset Icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -6,12 +6,14 @@ import ic_app_logo from "./ic_app_logo.svg";
 import ic_app_loading from "./ic_app_loading.svg";
 
 import {
-  UserOutlined
+  UserOutlined,
+  KeyOutlined
 } from '@ant-design/icons';
 
 export const Icons = {
   AppLogo,
-  UserOutlined
+  UserOutlined,
+  KeyOutlined
 };
 
 export const icons = {
