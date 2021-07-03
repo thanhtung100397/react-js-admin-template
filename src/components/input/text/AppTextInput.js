f6def118cc575c75c23cec9af62bbd190a9596e3
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-07-03 10:44:35 +0700

    Rename AppInput -> AppTextInput

---
+++ src/components/input/text/AppTextInput.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import classNames from 'classnames';
+import { baseProps, fromBaseProps } from '../../base';
+import { inputPropTypes, fromInputProps, useAppFormItem } from '../base';
+import { Input } from 'antd';
+import '../AppInput.scss';
+import './AppTextInput.scss';
+
+const propTypes = {
+  ...baseProps,
+  ...inputPropTypes
+};
+
+const defaultProps = {
+};
+
+const AppTextInput = (props) => {
+  const [ref, disabled] = useAppFormItem(props.disabled);
+  return (
+    <Input {...fromBaseProps({className: classNames('app-input', 'app-text-input')}, props)}
+           {...fromInputProps(props)}
+           ref={ref} disabled={disabled}/>
+  )
+};
+
+AppTextInput.propTypes = propTypes;
+
+AppTextInput.defaultProps = defaultProps;
+
+export default AppTextInput;
