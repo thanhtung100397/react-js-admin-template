37c000fc5f2b9ba8600cb15033c3dd397ea5ff46
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-07-03 13:59:53 +0700

    Add AppTextAreaInput component (group AppInput)

--- src/components/input/AppInput.js
+++ src/components/input/AppInput.js
@@ -1,9 +1,11 @@
 import AppTextInput from './text/AppTextInput';
 import AppPasswordInput from './password/AppPasswordInput';
+import AppTextAreaInput from './textarea/AppTextAreaInput';
 
 const AppInput = {
   Text: AppTextInput,
-  Password: AppPasswordInput
+  Password: AppPasswordInput,
+  TextArea: AppTextAreaInput
 };
 
 export default AppInput;
---
+++ src/components/input/textarea/AppTextAreaInput.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { baseProps, fromBaseProps } from '../../base';
+import { fromInputProps, inputPropTypes, useAppFormItem } from '../base';
+import { Input } from 'antd';
+import { isEmpty, TypeChecker } from '../../../utils/helpers';
+import '../AppInput.scss';
+import './AppTextAreaInput.scss';
+
+const { TextArea } = Input;
+
+const propTypes = {
+  ...baseProps,
+  ...inputPropTypes,
+  rows: PropTypes.number,
+  maxRows: PropTypes.number,
+  minRows: PropTypes.number,
+  autoRows: PropTypes.bool
+};
+
+const defaultProps = {
+};
+
+const AppTextAreaInput = (props) => {
+  const { maxRows, minRows, autoRows } = props;
+  const [ref, disabled] = useAppFormItem(props.disabled);
+  const [autoSize, setAutoSize] = useState();
+
+  useEffect(() => {
+    let autoSize;
+    if (TypeChecker.isBoolean(autoRows)) {
+      autoSize = autoRows;
+    } else {
+      autoSize = {};
+      if (maxRows) {
+        autoSize.maxRows = maxRows;
+      }
+      if (minRows) {
+        autoSize.minRows = minRows;
+      }
+      if (isEmpty(autoSize)) {
+        return;
+      }
+    }
+    setAutoSize(autoSize)
+  }, [maxRows, minRows, autoRows]);
+
+  return (
+    <TextArea {...fromBaseProps({className: classNames('app-input', 'app-text-area-input')}, props)}
+              {...fromInputProps(props)}
+              ref={ref} disabled={disabled} rows={props.rows} autoSize={autoSize}/>
+  )
+};
+
+AppTextAreaInput.propTypes = propTypes;
+
+AppTextAreaInput.defaultProps = defaultProps;
+
+export default AppTextAreaInput;
