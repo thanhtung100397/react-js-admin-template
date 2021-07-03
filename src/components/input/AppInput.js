58c6c2a7cfd55920b8d87ddeb20a73c7a589dfba
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-07-03 10:45:43 +0700

    Add base.js for AppInput and refactor all AppInput child components

--- src/components/input/AppInput.js
+++ src/components/input/AppInput.js
@@ -1,54 +1,7 @@
-import React, { useContext, useEffect, useState, useRef } from 'react';
-import PropTypes from 'prop-types';
-import { baseProps, fromBaseProps } from '../base';
-import { AppFormItemContext } from '../form/AppFormItem';
-import { Input } from 'antd';
-import './AppInput.scss';
+import AppTextInput from './text/AppTextInput';
 
-const propTypes = {
-  ...baseProps,
-  id: PropTypes.string,
-  value: PropTypes.oneOf([PropTypes.string, PropTypes.number]),
-  defaultValue: PropTypes.oneOf([PropTypes.string, PropTypes.number]),
-  placeholder: PropTypes.string,
-  icon: PropTypes.node,
-  onChange: PropTypes.func,
-  allowClear: PropTypes.bool,
-  maxLength: PropTypes.number,
-  disabled: PropTypes.bool
+const AppInput = {
+  Text: AppTextInput
 };
-
-const defaultProps = {
-  allowClear: false,
-  disabled: false
-};
-
-const AppInput = (props) => {
-  const { setInputRef } = useContext(AppFormItemContext);
-  const [disabled, setDisabled] = useState();
-  const ref = useRef({});
-
-  useEffect(() => {
-    setDisabled(props.disabled);
-  }, [props.disabled]);
-
-  useEffect(() => {
-    setInputRef && setInputRef({
-      getValue: () => ref.current?.state.value,
-      disable: (disabled) => setDisabled(disabled)
-    });
-  }, [setInputRef]);
-
-  return (
-    <Input {...fromBaseProps({className: 'app-input'}, props)}
-           ref={ref} id={props.id} allowClear={props.allowClear} maxLength={props.maxLength} disabled={disabled}
-           value={props.value} defaultValue={props.defaultValue} placeholder={props.placeholder}
-           onChange={props.onChange} prefix={props.icon}/>
-  )
-};
-
-AppInput.propTypes = propTypes;
-
-AppInput.defaultProps = defaultProps;
 
 export default AppInput;
---
+++ src/components/input/base.js
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import { useContext, useEffect, useRef, useState } from 'react';
+import { AppFormItemContext } from '../form/AppFormItem';
+
+export const inputPropTypes = {
+  id: PropTypes.string,
+  value: PropTypes.oneOf([PropTypes.string, PropTypes.number]),
+  defaultValue: PropTypes.oneOf([PropTypes.string, PropTypes.number]),
+  placeholder: PropTypes.string,
+  icon: PropTypes.node,
+  onChange: PropTypes.func,
+  allowClear: PropTypes.bool,
+  maxLength: PropTypes.number,
+  disabled: PropTypes.bool
+};
+
+export const fromInputProps = (props) => {
+  return {
+    id: props.id,
+    allowClear: props.allowClear,
+    maxLength: props.maxLength,
+    disabled: props.disabled,
+    value: props.value,
+    defaultValue: props.defaultValue,
+    placeholder: props.placeholder,
+    onChange: props.onChange,
+    prefix: props.icon
+  }
+};
+
+export const useAppFormItem = (disable) => {
+  const { setInputRef } = useContext(AppFormItemContext) || {};
+  const [disabled, setDisabled] = useState();
+  const ref = useRef({});
+
+  useEffect(() => {
+    setDisabled(disable);
+  }, [disable]);
+
+  useEffect(() => {
+    setInputRef && setInputRef({
+      getValue: () => ref.current?.state.value,
+      disable: (disabled) => setDisabled(disabled)
+    });
+  }, [setInputRef]);
+
+  return [ref, disabled]
+}
