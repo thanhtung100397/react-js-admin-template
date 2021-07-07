b83c29b71dab3252e7e801405eda9dc2b6d865e9
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-07-07 08:58:15 +0700

    Update display error message when unexpected error occurred in validation of AppFormItem

--- src/components/form/AppFormItemErrors.js
+++ src/components/form/AppFormItemErrors.js
@@ -1,21 +1,21 @@
 
 export class TransformValueError extends Error {
   constructor(cause) {
-    super('TRANSFORM VALUE ERROR');
+    super(`TRANSFORM VALUE ERROR: ${cause}`);
     this.cause = cause;
   }
 }
 
 export class ValidateValueError extends Error {
   constructor(cause) {
-    super('VALIDATE VALUE ERROR');
+    super(`VALIDATE VALUE ERROR: ${cause}`);
     this.cause = cause;
   }
 }
 
 export class TriggerValidationError extends Error {
   constructor(cause) {
-    super('TRIGGER VALIDATION ERROR');
+    super(`TRIGGER VALIDATION ERROR: ${cause}`);
     this.cause = cause;
   }
 }
