360092d9b864241ff86e8a96c263894cd53dc834
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-07-09 10:39:18 +0700

    Add flexContainer, rowGut, sidePadding to Prop of AppCol

--- src/containers/grid/column/AppCol.js
+++ src/containers/grid/column/AppCol.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { baseProps, fromBaseProps } from '../../../components/base';
 import { Col } from 'antd';
+import { isEmpty, TypeChecker } from '../../../utils/helpers';
 import './AppCol.scss';
+
+const SIDE_PADDING_DEFAULT = 6;
 
 const propTypes = {
   ...baseProps,
@@ -15,15 +18,45 @@ const propTypes = {
   md: PropTypes.number, // number of span cells in 'md' screen-size (screen ≥ 768px)
   lg: PropTypes.number, // number of span cells in 'lg' screen-size (screen ≥ 992px)
   xl: PropTypes.number, // number of span cells in 'xl' screen-size (screen ≥ 1200px)
-  xxl: PropTypes.number // number of span cells in 'xxl' screen-size (screen ≥ 1600px)
+  xxl: PropTypes.number, // number of span cells in 'xxl' screen-size (screen ≥ 1600px)
+
+  flexContainer: PropTypes.bool,
+  rowGut: PropTypes.number, // vertical spacing between rows, not work when flexContainer = false,
+  sidePadding: PropTypes.oneOfType([PropTypes.number, PropTypes.bool])
 };
 
 const defaultProps = {
 };
 
 const AppCol = (props) => {
+
+  const style = useMemo(() => {
+    let style = {};
+
+    if (props.flexContainer) {
+      return {
+        ...style,
+        display: 'flex',
+        flexDirection: 'column',
+        rowGap: props.rowGut
+      }
+    }
+
+    let sidePadding;
+    if (!TypeChecker.isUnset(props.sidePadding) && props.sidePadding !== false) {
+      sidePadding = TypeChecker.isNumber(props.sidePadding)? props.sidePadding : SIDE_PADDING_DEFAULT;
+      style = {
+        ...style,
+        paddingLeft: sidePadding,
+        paddingRight: sidePadding
+      }
+    }
+
+    return isEmpty(style)? undefined : style;
+  }, [props.flexContainer, props.rowGut, props.sidePadding]);
+
   return (
-    <Col {...fromBaseProps({ className: 'app-col' }, props)}
+    <Col {...fromBaseProps({ className: 'app-col', style: style }, props)}
          flex={props.flex} offset={props.offset} order={props.order} span={props.span}
          xs={props.xs} sm={props.sm} md={props.md} lg={props.lg} xl={props.lg} xxl={props.xxl}>
       {props.children}
