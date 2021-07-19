2e4efbaf098176efceb290dc6b1a62f832e77d9b
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-07-19 15:37:32 +0700

    Add baseClassName to base.js of AppTypography

--- src/components/typography/base.js
+++ src/components/typography/base.js
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { fromBaseProps } from '../base';
 import { TypeChecker } from '../../utils/helpers';
 
 export const typographyPropTypes = {
@@ -10,8 +12,15 @@ export const typographyPropTypes = {
   strikethrough: PropTypes.bool, // strike through line style
   highlight: PropTypes.bool, // highlight style
   disabled: PropTypes.bool, // disabled content
-  onClick: PropTypes.func
+  onClick: PropTypes.func,
+  firstCap: PropTypes.bool, // display first character uppercase
+  allCaps: PropTypes.bool // display all characters uppercase
 };
+
+export const typographyBaseClassNames = (props) => ({
+  'all-caps': props.allCaps,
+  'first-caps': props.firstCap
+});
 
 const italicValue = (italic) => { // ant design has bug about Typography italic prop
   if (TypeChecker.isBoolean(italic)) {
@@ -19,16 +28,20 @@ const italicValue = (italic) => { // ant design has bug about Typography italic 
   }
 };
 
-export const fromTypographyProps = (props) => {
-  return {
-    strong: props.bold,
-    italic: italicValue(props.italic),
-    underline: props.underline,
-    ellipsis: props.ellipsis,
-    copyable: props.copyable,
-    delete: props.strikethrough,
-    mark: props.highlight,
-    disabled: props.disabled,
-    onClick: props.onClick
-  }
-};
+export const fromTypographyBaseProps = (startedValues = {}, props) => ({
+  ...fromBaseProps({
+    className: classNames({
+      ...typographyBaseClassNames(props),
+      [startedValues.className]: startedValues.className
+    }),
+  }, props),
+  strong: props.bold,
+  italic: italicValue(props.italic),
+  underline: props.underline,
+  ellipsis: props.ellipsis,
+  copyable: props.copyable,
+  delete: props.strikethrough,
+  mark: props.highlight,
+  disabled: props.disabled,
+  onClick: props.onClick
+});
