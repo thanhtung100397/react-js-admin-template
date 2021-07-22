1382c957a6dad6b85c0d7f324008ba77f0c9ca8e
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-07-22 22:09:32 +0700

    Add storageHelpers for handling with all storage types

---
+++ src/utils/storageHelpers.js
@@ -0,0 +1,17 @@
+
+export const LocalStorage = {
+  get: (key, defaultValue) => localStorage.getItem(key) || defaultValue,
+
+  save: (key, value) => localStorage.setItem(key, value),
+  getObject: (key, defaultValue) => {
+    try {
+      return JSON.parse(this.get(key, defaultValue))
+    } catch (e) {
+      return defaultValue || {}
+    }
+  },
+  saveObject: (key, obj) => this.save(key, JSON.stringify(obj)),
+  remove: (key) => localStorage.removeItem(key),
+  clear: () => localStorage.clear(),
+  has: (key) => Boolean(this.get(key, false))
+}
