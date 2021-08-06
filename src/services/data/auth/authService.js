cf4195f6455e871af23e2ef63f306cd6563c7057
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-08-06 11:40:50 +0700

    Replace signInService by authService

---
+++ src/services/data/auth/authService.js
@@ -0,0 +1,12 @@
+import { appApiClient } from '../../apiClient';
+
+export const signIn = async (username, password) => {
+  return await appApiClient.request({
+    method: 'POST',
+    url: 'api/authentication/username-password',
+    data: {
+      username: username,
+      password: password
+    }
+  });
+};
