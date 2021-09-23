149ed78f28b7032bbb3187239d06c547ad86c6f1
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-09-24 06:52:33 +0700

    Add mock database schema and data

---
+++ mock_server/mock-database-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user(id, username, password, first_name, last_name)
+VALUES('2aa6f6d3-e188-46ad-b38e-d1309412e4f7', 'admin', '123456', 'Admin', 'Super');
---
+++ mock_server/mock-database-schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS user(
+   id               VARCHAR(36)     NOT NULL PRIMARY KEY,
+   username         VARCHAR(255)    NOT NULL,
+   password         VARCHAR(255)    NOT NULL,
+   first_name       VARCHAR(35)     NOT NULL,
+   last_name        VARCHAR(70)     NOT NULL,
+   created_date     DATETIME        NOT NULL                DEFAULT CURRENT_TIMESTAMP
+);
