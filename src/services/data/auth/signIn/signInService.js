093d3c209ebb031a560616e1c94af8625fdd54c0
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-09-28 17:41:48 +0700

    Update sign in api path in signInService

--- src/services/data/auth/signIn/signInService.js
+++ src/services/data/auth/signIn/signInService.js
@@ -3,11 +3,7 @@ import { appApiClient } from '../../../apiClient';
 export const signIn = async (username, password) => {
   return await appApiClient.request({
     method: 'POST',
-    url: 'api/oauth2/authentication/username-password',
-    headers: {
-      client_id: process.env.REACT_APP_OAUTH2_CLIENT_ID,
-      secret: process.env.REACT_APP_OAUTH2_CLIENT_SECRET
-    },
+    url: 'api/authentication/username-password',
     data: {
       username: username,
       password: password
