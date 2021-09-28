d40a7076a54ac53a1d7dd73d3092a60f4524e44b
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-09-28 16:53:52 +0700

    [Mock Server] Add api get list users and get user by id

--- mock_server/src/api/user/userApis.js
+++ mock_server/src/api/user/userApis.js
@@ -1,15 +1,56 @@
 const { db } = require('../../database/dbClient');
+const { pagination } = require('../../constants/responses');
+const { PaginationQuery, Where } = require('../../helpers/queryHelpers');
+const { AppResponses } = require('../../constants/responses');
+const { isNotEmpty } = require('../../helpers/stringHelpers');
 
 exports.userApis = [
   {
     method: 'GET',
-    path: '/api/user/info',
+    path: '/api/users',
     authRequired: true,
     handle: async (req, res) => {
-      res.jsonResponse({
-        name: 'abc',
-        age: 1
-      })
+      const { username, banned } = req.query;
+      const { offset, limit } = pagination(req);
+
+      const { whereQuery, whereParams } = new Where()
+        .condition('username = $username', {
+          $username: username
+        }).if(isNotEmpty(username))
+        .and()
+        .condition('banned = $banned', {
+          $banned: banned
+        }).if(isNotEmpty(banned))
+        .toQuery();
+
+      const pageUsers = await new PaginationQuery(
+        `SELECT id, username, first_name, last_name, banned, created_date FROM user ${whereQuery}`,
+        whereParams
+      ).withPagination(offset, limit)
+        .execute();
+
+      res.jsonResponse(pageUsers);
+    }
+  },
+  {
+    method: 'GET',
+    path: '/api/users/:userId',
+    authRequired: true,
+    handle: async (req, res) => {
+      const { userId } = req.params;
+
+      const user = await db.getAll(
+        'SELECT id, username, first_name, last_name, banned, created_date FROM user WHERE id = $userId',
+        {
+          $userId: userId
+        }
+      );
+
+      if (!user) {
+        throw new AppResponses.USER_NOT_FOUND
+      }
+
+      res.jsonResponse(user)
     }
   }
 ];
