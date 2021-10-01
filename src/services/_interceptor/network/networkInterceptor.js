0e855f48159509e547fcb1f0d6d239fc4b9c6bac
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-10-01 17:43:37 +0700

    Update no network error interceptor

--- src/services/_interceptor/network/networkInterceptor.js
+++ src/services/_interceptor/network/networkInterceptor.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 import AppNotification from '../../../components/notification/AppNotification';
+import { appIntl } from '../../../translations/provider/TranslationsProvider';
 
 const isNetworkError = (error) => {
   return error.message === 'Network Error';
-}
+};
 
 export const networkInterceptor = (apiInstance) => {
   apiInstance.interceptors.response.use(
@@ -13,7 +13,10 @@ export const networkInterceptor = (apiInstance) => {
     },
     (error) => {
       if (isNetworkError(error)) {
-        AppNotification.error(<FormattedMessage id="ID_NETWORK_ERROR_TITLE"/>, <FormattedMessage id="ID_NETWORK_ERROR_MESSAGE"/>)
+        AppNotification.error(
+          appIntl.getMessage({id: 'ID_NETWORK_ERROR_TITLE'}),
+          appIntl.getMessage({id: 'ID_NETWORK_ERROR_MESSAGE'})
+        )
       }
       return Promise.reject(error);
     });
