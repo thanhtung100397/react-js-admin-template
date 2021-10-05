834fa066fd7d47f4cbb1bebac83e871406155e2e
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-10-05 17:44:38 +0700

    Add authServer and apiHelpers

--- src/services/apiHelpers.js
+++ src/services/apiHelpers.js
@@ -2,3 +2,23 @@
 export const isSuccess = (res) => {
   return res.httpStatus === 200;
 };
+
+export const getHttpStatus = (res) => {
+  return res.httpStatus;
+};
+
+export const getHeaders = (res) => {
+  return res.headers;
+};
+
+export const getCode = (res) => {
+  return res.data?.code;
+};
+
+export const getMessage = (res) => {
+  return res.data?.msg;
+};
+
+export const getData = (res) => {
+  return res.data?.data;
+};
---
+++ src/services/data/auth/authService.js
@@ -0,0 +1,45 @@
+import { LocalStorage } from '../../../utils/storageHelpers';
+
+const STORAGE_AUTH_USER_ID_KEY = 'auth.user_id';
+const STORAGE_AUTH_USERNAME_KEY = 'auth.username';
+const STORAGE_AUTH_ACCESS_TOKEN_KEY = 'auth.access_token';
+const STORAGE_AUTH_REFRESH_TOKEN_KEY = 'auth.refresh_token';
+
+export const isAuth = () => {
+  return Boolean(getAuthUserId());
+};
+
+export const getAuthUserId = () => {
+  LocalStorage.get(STORAGE_AUTH_USER_ID_KEY);
+};
+
+export const getAuthUsername = () => {
+  LocalStorage.get(STORAGE_AUTH_USERNAME_KEY);
+};
+
+export const getAuthAccessToken = () => {
+  LocalStorage.get(STORAGE_AUTH_ACCESS_TOKEN_KEY);
+};
+
+export const getAuthRefreshToken = () => {
+  LocalStorage.get(STORAGE_AUTH_REFRESH_TOKEN_KEY);
+};
+
+export const getAuthInfo = () => {
+  if (!isAuth()) {
+    return;
+  }
+  return {
+    userId: getAuthUserId(),
+    username: getAuthUsername(),
+    accessToken: getAuthAccessToken(),
+    refreshToken: getAuthRefreshToken()
+  }
+};
+
+export const saveAuthInfo = ({userId, username, accessToken, refreshToken}) => {
+  LocalStorage.save(STORAGE_AUTH_USER_ID_KEY, userId);
+  LocalStorage.save(STORAGE_AUTH_USERNAME_KEY, username);
+  LocalStorage.save(STORAGE_AUTH_ACCESS_TOKEN_KEY, accessToken);
+  LocalStorage.save(STORAGE_AUTH_REFRESH_TOKEN_KEY, refreshToken);
+};
