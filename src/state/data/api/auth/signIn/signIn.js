5323aefc8e3323ea1af3e3ca72df34ec189033a2
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-10-08 17:15:19 +0700

    Refactor and update UNIQUE_ID = store path of signIn

--- src/state/data/api/auth/signIn/signIn.js
+++ src/state/data/api/auth/signIn/signIn.js
@@ -3,21 +3,21 @@ import { createApiSagas } from '../../apiSaga';
 import { createApiReducer } from '../../apiReducer';
 import { signIn } from '../../../../../services/data/auth/signIn/signInService';
 
-const UNIQUE_ID = 'DATA.API.AUTH.SIGN_IN';
+const UNIQUE_ID = 'data.api.auth.signIn';
 
 const apiCall = async (data) => {
   const { username, password } = data;
   return await signIn(username, password);
 };
 
-const signInAction = createApiActions(UNIQUE_ID);
+const SignInAction = createApiActions(UNIQUE_ID);
 
 const signInReducer = createApiReducer(UNIQUE_ID);
 
-const signInSaga = createApiSagas(UNIQUE_ID, apiCall, signInAction);
+const signInSaga = createApiSagas(UNIQUE_ID, apiCall, SignInAction);
 
 export {
-  signInAction,
+  SignInAction,
   signInReducer,
   signInSaga
 }
