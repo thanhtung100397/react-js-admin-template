e426ebfec97a6f004e964acc1b82f3de4b96a98e
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-10-08 17:16:00 +0700

    Add useApiFetchingWatcher for apiHook

---
+++ src/state/data/api/apiHook.js
@@ -0,0 +1,19 @@
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { getObjField } from '../../../utils/helpers';
+import { FETCHING_STATUS_FIELD, FetchingStatus } from './apiReducer';
+
+export const useApiFetchingWatcher = (action = {}) => {
+
+  const fetchingStatus = useSelector((state) => getObjField(state, `${action.path}.${action.id}.${FETCHING_STATUS_FIELD}`));
+
+  return useMemo(() => ({
+    isIdle: !fetchingStatus || fetchingStatus === FetchingStatus.IDLE,
+    isInProgress: fetchingStatus === FetchingStatus.IN_PROGRESS,
+    isError: fetchingStatus === FetchingStatus.ERROR
+  }), [fetchingStatus]);
+};
+
+export const useApiResultWatcher = (action) => {
+
+};
