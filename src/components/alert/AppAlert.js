29165535cd97126e03a298ebea5d3a2c82998d50
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-10-11 10:49:07 +0700

    Add autoTextColor prop to AppAlert component

--- src/components/alert/AppAlert.js
+++ src/components/alert/AppAlert.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { baseProps, fromBaseProps } from '../base';
 import { Alert } from 'antd';
+import AppText from '../typography/text/AppText';
 import './AppAlert.scss';
 
 const AlertType = {
@@ -16,6 +17,7 @@ const propTypes = {
   message: PropTypes.node,
   description: PropTypes.string,
   type: PropTypes.oneOf(Object.keys(AlertType).map((key) => AlertType[key])),
+  autoTextColor: PropTypes.bool,
   action: PropTypes.node,
   onClose: PropTypes.func,
   afterClose: PropTypes.func,
@@ -27,12 +29,32 @@ const propTypes = {
 };
 
 const defaultProps = {
+  autoTextColor: true
+};
+
+const appAlertText = (type, text) => {
+  return <AppText className={`alert-text-${type}`}>{text}</AppText>
 };
 
 const AppAlert = (props) => {
+
+  const message = useMemo(() => {
+    if (props.autoTextColor) {
+      return appAlertText(props.type, props.message);
+    }
+    return props.message;
+  }, [props.autoTextColor, props.type, props.message]);
+
+  const description = useMemo(() => {
+    if (props.autoTextColor) {
+      return appAlertText(props.type, props.description);
+    }
+    return props.description;
+  }, [props.autoTextColor, props.type, props.description]);
+
   return (
     <Alert {...fromBaseProps({ className: 'app-alert' }, props)}
-           message={props.message} description={props.description} type={props.type}
+           message={message} description={description} type={props.type}
            action={props.action} onClose={props.onClose} afterClose={props.afterClose}
            baner={props.banner} closable={props.closable} closeText={props.closeText}
            showIcon={props.showIcon} icon={props.icon}/>
