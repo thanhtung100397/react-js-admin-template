bc62004d72107460877b73a30641d346d672ed11
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-10-11 18:04:34 +0700

    Update apiClient networkInterceptor: no internet error and server refused connection error

--- src/services/_interceptor/network/networkInterceptor.js
+++ src/services/_interceptor/network/networkInterceptor.js
@@ -1,5 +1,7 @@
 import AppNotification from '../../../components/notification/AppNotification';
 import { appIntl } from '../../../translations/provider/TranslationsProvider';
+import { hasInternetConnection } from '../../../utils/browserHelpers';
+import { CUSTOM_ERROR_MESSAGE_FIELD } from '../../apiClient';
 
 const isNetworkError = (error) => {
   return error.message === 'Network Error';
@@ -12,10 +14,16 @@ export const networkInterceptor = (apiInstance) => {
     },
     (error) => {
       if (isNetworkError(error)) {
-        AppNotification.error(
-          appIntl.getMessage({id: 'ID_NETWORK_ERROR_TITLE'}),
-          appIntl.getMessage({id: 'ID_NETWORK_ERROR_MESSAGE'})
-        )
+        let errorTitle, errorMessage;
+        if (hasInternetConnection()) {
+          errorTitle = appIntl.getMessage({id: 'ID_NETWORK_ERROR_SERVER_REFUSED_CONNECTION_TITLE'});
+          errorMessage = appIntl.getMessage({id: 'ID_NETWORK_ERROR_SERVER_REFUSED_CONNECTION_MESSAGE'});
+        } else {
+          errorTitle = appIntl.getMessage({id: 'ID_NETWORK_ERROR_NO_INTERNET_TITLE'});
+          errorMessage = appIntl.getMessage({id: 'ID_NETWORK_ERROR_NO_INTERNET_MESSAGE'});
+        }
+        AppNotification.error(errorTitle, errorMessage);
+        error[CUSTOM_ERROR_MESSAGE_FIELD] = `${errorTitle}: ${errorMessage}`;
       }
       return Promise.reject(error);
     });
---
+++ src/utils/browserHelpers.js
@@ -0,0 +1,2 @@
+
+export const hasInternetConnection = () => window.navigator.onLine;
