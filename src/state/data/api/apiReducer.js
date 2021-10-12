54117df42bbbaa1be8fec90acb6523d8e78874a3
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-10-12 10:34:39 +0700

    Update apiReducer state (add httpStatusText field, fix isFailure assign error)

--- src/state/data/api/apiReducer.js
+++ src/state/data/api/apiReducer.js
@@ -1,8 +1,9 @@
 import {
-  getCode, getData, getHeaders, getHttpStatus, getMessage, isSuccess, getErrorMessage, getErrorType
+  getCode, getData, getHeaders, getHttpStatus, getMessage, isSuccess, getErrorMessage, getErrorType, getHttpStatusText
 } from '../../../services/apiHelpers';
 import {
-  API_FETCHING_ACTION, API_RESPONSE_SUCCESS_ACTION, API_RESPONSE_FAILURE_ACTION, API_FETCHING_ERROR_ACTION, UNEXPECTED_REDUCER_ERROR_ACTION
+  API_FETCHING_ACTION, API_RESPONSE_SUCCESS_ACTION, API_RESPONSE_FAILURE_ACTION, API_FETCHING_ERROR_ACTION,
+  UNEXPECTED_REDUCER_ERROR_ACTION
 } from '../../actionTypes';
 import { updateState, updateStateField } from '../../stateHelpers';
 
@@ -14,24 +15,29 @@ export const FetchingStatus = {
   ERROR: 'ERROR',
 };
 
-export const apiResToState = (res) => ({
-  [FETCHING_STATUS_FIELD]: FetchingStatus.IDLE,
-  isSuccess: isSuccess(res),
-  isFailure: !this.isSuccess, // this field is NOT redundant
-  httpStatus: getHttpStatus(res),
-  headers: getHeaders(res),
-  code: getCode(res),
-  message: getMessage(res),
-  data: getData(res),
-  error: undefined,
-  errorType: undefined
-});
+export const apiResToState = (res) => {
+  const success = isSuccess(res);
+  return {
+    [FETCHING_STATUS_FIELD]: FetchingStatus.IDLE,
+    isSuccess: success,
+    isFailure: !success, // this field is NOT redundant
+    httpStatus: getHttpStatus(res),
+    httpStatusText: getHttpStatusText(res),
+    headers: getHeaders(res),
+    code: getCode(res),
+    message: getMessage(res),
+    data: getData(res),
+    error: undefined,
+    errorType: undefined
+  }
+};
 
 export const apiFetchingErrorToState = (error) => ({
   [FETCHING_STATUS_FIELD]: FetchingStatus.ERROR,
-  isSuccess: false,
-  isFailure: false, // this field is NOT redundant
+  isSuccess: undefined,
+  isFailure: undefined, // this field is NOT redundant
   httpStatus: undefined,
+  httpStatusText: undefined,
   headers: undefined,
   code: undefined,
   message: getErrorMessage(error),
