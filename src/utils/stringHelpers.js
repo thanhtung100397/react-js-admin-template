a2e7886f6ce2f53508a684377a360fbb283df21a
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-10-29 17:45:32 +0700

    Add stringHelpers.isEmpty() and isBlank()

--- src/utils/stringHelpers.js
+++ src/utils/stringHelpers.js
@@ -2,3 +2,11 @@
 export const stringJoin = (delim, ...values) => {
   return values.filter(value => value).join(delim);
 };
+
+export const isEmpty = (value) => {
+  return !value;
+};
+
+export const isBlank = (value) => {
+  return !value || !value.trim();
+};
