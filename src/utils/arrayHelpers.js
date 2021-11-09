14e8b0abb0dfe23b988029a1688b735d78107ee4
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-11-09 17:56:12 +0700

    Update ArrayHelpers.intersectValues, nonIntersectValues, removeValues

--- src/utils/arrayHelpers.js
+++ src/utils/arrayHelpers.js
@@ -1,5 +1,19 @@
 
 export const ArrayHelpers = {
-  addValues: (array, values) => arr.concat(values),
+  addValues: (array, values) => array.concat(values),
   removeValuesByIndex: (array, index, removeCount = 1) => [...array].splice(array, index, removeCount),
+  intersectValues: (...arrays) =>
+    arrays.reduce((preArray, curArray) => {
+      if (curArray && curArray.length) {
+        return preArray.filter((item) => curArray.includes(item));
+      }
+      return [];
+    }),
+  nonIntersectValues: (...arrays) =>
+    arrays.reduce((preArray, curArray) => {
+      if (curArray && curArray.length) {
+        return preArray.filter((item) => !curArray.includes(item));
+      }
+      return preArray;
+    })
 };
