f4ab9c8ed2a1d9de99767bff3e445ff8c6e67ca4
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-11-12 14:07:57 +0700

    Update ArrayHelpers.intersectValues() and .nonIntersectValues()

--- src/utils/arrayHelpers.js
+++ src/utils/arrayHelpers.js
@@ -1,19 +1,8 @@
+import { intersection, xor } from 'lodash';
 
 export const ArrayHelpers = {
   addValues: (array, values) => array.concat(values),
   removeValuesByIndex: (array, index, removeCount = 1) => [...array].splice(array, index, removeCount),
-  intersectValues: (...arrays) =>
-    arrays.reduce((preArray, curArray) => {
-      if (curArray && curArray.length) {
-        return preArray.filter((item) => curArray.includes(item));
-      }
-      return [];
-    }),
-  nonIntersectValues: (...arrays) =>
-    arrays.reduce((preArray, curArray) => {
-      if (curArray && curArray.length) {
-        return preArray.filter((item) => !curArray.includes(item));
-      }
-      return preArray;
-    })
+  intersectValues: (...arrays) => intersection(...arrays),
+  nonIntersectValues:  (...arrays) => xor(...arrays)
 };
