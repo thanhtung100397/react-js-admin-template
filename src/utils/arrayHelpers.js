57ba3503478ab576861b2667227a7a0e7b342255
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-11-21 21:03:16 +0700

    Update ArrayHelpers.addValues() and add removeValue()

--- src/utils/arrayHelpers.js
+++ src/utils/arrayHelpers.js
@@ -1,8 +1,9 @@
-import { intersection, xor } from 'lodash';
+import { intersection, xor, filter, concat } from 'lodash';
 
 export const ArrayHelpers = {
-  addValues: (array, values) => array.concat(values),
+  addValues: (array, ...values) => concat(array || [], ...values),
   removeValuesByIndex: (array, index, removeCount = 1) => [...array].splice(array, index, removeCount),
+  removeValue: (array, value) => filter(array, (item) => item !== value),
   intersectValues: (...arrays) => intersection(...arrays),
   nonIntersectValues:  (...arrays) => xor(...arrays)
 };
