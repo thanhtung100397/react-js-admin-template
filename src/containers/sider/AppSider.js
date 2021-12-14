92b6a5da3f0a5bedaace4410debfe2ea5f36658f
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-12-15 00:44:02 +0700

    Add collapsedWidth, hideCollapseButton and update collapse logic in AppSider component

--- src/containers/sider/AppSider.js
+++ src/containers/sider/AppSider.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { baseProps, fromBaseProps } from '../../components/base';
 import { Layout } from 'antd';
 import './AppSider.scss';
+import { TypeChecker } from '../../utils/helpers';
 
 export const SiderTheme = {
   LIGHT: 'light',
@@ -13,9 +14,11 @@ const propTypes = {
   ...baseProps,
   theme: PropTypes.oneOf(Object.keys(SiderTheme).map((key) => SiderTheme[key])),
   width: PropTypes.number,
+  collapsedWidth: PropTypes.number,
   collapsible: PropTypes.bool,
   collapsed: PropTypes.bool,
-  onCollapse: PropTypes.func,
+  onCollapse: PropTypes.func, // (collapsed: boolean) => {}
+  hideCollapseButton: PropTypes.bool
 };
 
 const defaultProps = {
@@ -23,13 +26,24 @@ const defaultProps = {
 };
 
 const AppSider = (props) => {
-  const [collapsed, setCollapsed] = useState();
+  const [collapsed, setCollapsed] = useState(props.collapsed);
+
+  const isCollapsed = TypeChecker.isUnset(props.collapsed)? collapsed : props.collapsed;
+
+  const onCollapse = (collapsed) => {
+    if (TypeChecker.isUnset(props.collapsed)) {
+      setCollapsed(collapsed);
+    }
+    if (props.onCollapse) {
+      props.onCollapse(collapsed);
+    }
+  };
 
   return (
     <Layout.Sider {...fromBaseProps({ className: 'app-sider' }, props)}
-                  theme={props.theme} width={props.width}
-                  collapsible={props.collapsible} collapsed={collapsed}
-                  onCollapse={props.onCollapse}>
+                  theme={props.theme} width={props.width} collapsedWidth={props.collapsedWidth}
+                  collapsible={props.collapsible} collapsed={isCollapsed} onCollapse={onCollapse}
+                  trigger={props.hideCollapseButton? null : undefined}>
       {props.children}
     </Layout.Sider>
   )
