743714095fd271ebbf81fef406739b08f2ac6f6a
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-12-15 00:46:00 +0700

    Complete add AppLogo and AppName to sider in Dashboard page

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,35 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppContainer from '../../containers/container/AppContainer';
 import AppSider from '../../containers/sider/AppSider';
 import AppHeader from '../../containers/header/AppHeader';
 import AppContent from '../../containers/content/AppContent';
-import AppFooter from '../../containers/footer/AppFooter';
 import AppMenu from '../../components/menu/AppMenu';
 import AppImage from '../../components/image/AppImage';
 import AppTypography from '../../components/typography/AppTypography';
 import { dashboardMenu } from './dashboardMenu';
 import { images } from '../../assets/images';
+import { delay } from '../../utils/helpers';
 import './Dashboard.scss';
 
 const { Title } = AppTypography;
 
+const SIDER_COLLAPSED_ANIMATION_DELAY_MILLIS = 200;
+
 const Dashboard = (props) => {
+  const [siderCollapsed, setSiderCollapsed] = useState();
+  const [hideAppName, setHideAppName] = useState();
+
+  useEffect(() => {
+    const updateHideAppName = async (collapsed) => {
+      if (!collapsed) {
+        await delay(SIDER_COLLAPSED_ANIMATION_DELAY_MILLIS);
+      }
+      setHideAppName(collapsed);
+    }
+    updateHideAppName(siderCollapsed);
+  }, [siderCollapsed]);
+
   return (
     <AppContainer className="dashboard-page wh-full">
-      <AppSider width={256}>
+      <AppSider width={256} collapsible={true} collapsedWidth={64}
+                collapsed={siderCollapsed} onCollapse={setSiderCollapsed}>
         <div className="app-logo-container">
           <AppImage src={images.img_app_logo} square={true} height="100%"/>
-          <Title level={5}>{process.env.REACT_APP_APP_NAME}</Title>
+          {
+            !hideAppName && <Title level={4}>{process.env.REACT_APP_APP_NAME}</Title>
+          }
         </div>
-        <AppMenu items={dashboardMenu} expandAll={true} expandAllLevel={1}/>
+        <AppMenu items={dashboardMenu} expandCurrentOnly={true}/>
       </AppSider>
       <AppContainer>
         <AppHeader>
+
         </AppHeader>
         <AppContent>
         </AppContent>
-        <AppFooter>
-        </AppFooter>
       </AppContainer>
     </AppContainer>
   )
